446c4b1538439282b029629ff23fb9aff09e137e
thaissa <thaiissa.muniiz@gmail.com>
2023-03-27 16:44:26 -0300

    add ticket tablet screen

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import Header from './components/Header';
 // import Banner from './components/Banner';
 // import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-// import TicketScreen from './components/TicketScreen';
+import TicketScreen from './components/TicketScreen';
 
 // import ImageDn from './components/ImageDn';
 // import bannerImg from './img/Banner - Homepage.svg';
 // import ExperienceScreen from './components/ExperienceScreen';
 // import SectorMap from './components/SectorMap';
 // import InfoScreen from './components/InfoScreen';
-import BuyTicketScreen from './components/BuyTicketScreen';
+// import BuyTicketScreen from './components/BuyTicketScreen';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <BuyTicketScreen />
+      <TicketScreen />
       <Footer />
     </div>
   );
